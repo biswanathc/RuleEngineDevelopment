495e1fc7b9a90f0926f9e0c60c59008517add136
Biswanath Chatterjee <biswanath.chatterjee1@gmail.com>
2020-07-01 23:08:06 +0530

    Added classes for Rule and skeleton

---
+++ RuleEngine/PaymentInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine
+{
+    public class PaymentInfo
+    {
+        public int PaymentId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string PaymentType { get; set; }
+    }
+}
---
+++ RuleEngine/RuleEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine
+{
+    public class RuleEvaluator
+    {
+        public void InvokeRules()
+        {
+
+        }
+
+        private void GetRuleList()
+        {
+            List<Rule> ruleList = new List<Rule>();
+            ruleList.Add(new Rule() { RuleName = "PhysicalProduct", MethodName = "GeneratePackagingSlipShipping;GenerateCommision" });
+            ruleList.Add(new Rule() { RuleName = "Book", MethodName = "GeneratePackagingSlipRoyalty;GenerateCommision" });
+            ruleList.Add(new Rule() { RuleName = "Membership", MethodName = "ActivateMembership;SendMail" });
+            ruleList.Add(new Rule() { RuleName = "UpgradeMembership", MethodName = "UpgradeMembership;SendMail" });
+            ruleList.Add(new Rule() { RuleName = "SpecialVideo", MethodName = "FreeVideo" });
+        }
+    }
+}
