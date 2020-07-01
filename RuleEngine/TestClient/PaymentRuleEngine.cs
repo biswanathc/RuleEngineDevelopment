47d0ec7b52d4b4ddd5ff87120614f029a531f6ca
Biswanath Chatterjee <biswanath.chatterjee1@gmail.com>
2020-07-01 23:46:04 +0530

    Added logic for dynamic method invoke

---
+++ RuleEngine/TestClient/PaymentRuleEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleEngine
+{
+    public class PaymentRuleEngine
+    {
+        public void InvokeRules(PaymentInfo paymentInfo)
+        {
+            RuleEvaluator re = new RuleEvaluator();
+            List<string> methodList = new List<string>();
+            var qualifiedRules = re.EvaluateQualifyingRules(paymentInfo);
+            StringBuilder sb = new StringBuilder();
+            foreach (Rule rule in qualifiedRules)
+            {
+                sb.Append(rule.MethodName);
+            }
+
+            methodList = sb.ToString().Split(';').ToList();
+            var count = methodList.Count;
+            methodList.RemoveAt(--count);
+
+            foreach (string methodName in methodList)
+            {
+                MethodExecutor me = new MethodExecutor();
+                Type methodType = me.GetType();
+                System.Reflection.MethodInfo method = methodType.GetMethod(methodName);
+                object magicValue = method.Invoke(me, new object[] { paymentInfo.PaymentId });
+            }
+        }
+    }
+}
---
+++ RuleEngine/TestClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuleEngine;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentInfo info = new PaymentInfo() { PaymentId = 1, OrderId = 100, PaymentType = "Book",CustomerId = 1000 };
+            RuleEvaluator re = new RuleEvaluator();
+            re.InvokeRules(info);
+        }
+    }
+}
